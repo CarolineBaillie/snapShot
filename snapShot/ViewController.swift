af85abd44f75e20b9e89f58b21297ca2553de5bb
CarolineBaillie <caroline@bailliefamily.com>
2020-12-27 08:52:03 -0500

    Initial Commit

---
+++ snapShot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  snapShot
+//
+//  Created by Caroline Baillie on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
